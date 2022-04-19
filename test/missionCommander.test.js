ee59361cc95ee0b8d7d6fa6e434c969e8d3b5add
unciafidelis <unciafidelis@gmail.com>
2022-04-19 09:56:59 -0500

    fix: missused jest function TestWatcher to test

--- test/missionCommander.test.js
+++ test/missionCommander.test.js
@@ -1,8 +1,6 @@
-const { TestWatcher } = require("jest")
-
 describe("Esto es una suite de pruebas", () => {
-    TestWatcher('Caso de prueba 1', () => {
-        const result = 1 + 2
-        expect(result).toBe(10)
-    })
-})
+    test('Caso de prueba 1', () => {
+      const result = 1 + 2 
+      expect(result).toBe(10);
+    });
+  })
