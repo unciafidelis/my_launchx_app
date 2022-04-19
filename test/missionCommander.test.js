4623c6668e1e9c12d79f2d386cfae77583313719
unciafidelis <unciafidelis@gmail.com>
2022-04-19 10:03:52 -0500

    Add: Class MissionCommander error expected

--- test/missionCommander.test.js
+++ test/missionCommander.test.js
@@ -1,6 +1,8 @@
-describe("Esto es una suite de pruebas", () => {
-    test('Caso de prueba 1', () => {
-      const result = 1 + 2 
-      expect(result).toBe(10);
+const MissionCommander = require('./../app/missionCommander');
+
+describe("Unit Tests for Mission Commander Class", () => {
+    test('1) Create a mission commander object', () => {
+      const myMissionCommander = new MissionCommander("Woopa")
+      expect(myMissionCommander.name).toBe("Woopaa");
     });
   })
