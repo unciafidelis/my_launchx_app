6158f766c65ee1fe2f4ee1dd5e15c18370020da9
unciafidelis <unciafidelis@gmail.com>
2022-04-19 10:05:08 -0500

    Fix: Class MissionCommander test passed

--- test/missionCommander.test.js
+++ test/missionCommander.test.js
@@ -3,6 +3,6 @@ const MissionCommander = require('./../app/missionCommander');
 describe("Unit Tests for Mission Commander Class", () => {
     test('1) Create a mission commander object', () => {
       const myMissionCommander = new MissionCommander("Woopa")
-      expect(myMissionCommander.name).toBe("Woopaa");
+      expect(myMissionCommander.name).toBe("Woopa");
     });
   })
